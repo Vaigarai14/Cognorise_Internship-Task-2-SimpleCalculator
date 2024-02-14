408fd7a9983cf270a4909db3ba205c668eb3de39
Vaigarai14 <vaigaraithennarasu144@gmail.com>
2024-02-14 17:52:16 +0530

    added some features

--- script.js
+++ script.js
@@ -9,68 +9,17 @@ const lightmode = () => {
     githubcolor.classList.toggle("iconcolor");
 };
 
-// function getNumber(num) {
-//   inputfield.value += num;
-//   console.log(num);
-// }
-
-// function getOperator(operator) {
-//   if (operator === ".") {
-//     if (!inputfield.value.includes(".")) {
-//       inputfield.value += operator;
-//     } else {
-//       inputfield.value += " " + operator + " ";
-//     }
-//   }
-//   console.log(operator);
-// }
-
-// function Calculate() {
-//   const value = inputfield.value;
-//   a = value.split(" ");
-//   console.log(a);
-// }
-
-// function clearitem() {
-//   inputfield.value = "";
-// }
-
-// function arithmetic() {
-//   numberbtn.forEach((element) => {
-//     element.addEventListener("click", (event) => {
-//       if (event.target.textContent != "=") {
-//         inputfield.style.textAlign = "right";
-//         inputfield.value += event.target.textContent;
-//         const operands = inputfield.value.split(/[\+\-\x\/]/);
-//         const operators = inputfield.value.match(/[\+\-\x\/]/);
-//         sum = 0;
-//         for (var i = 0; i < operands.length; i++) {
-//           let first = operands[i];
-//           if (operators) {
-//             console.log();
-//             sum = parseFloat(first) + parseFloat(sum);
-//             console.log(sum);
-//             inputfield.value = sum;
-//           }
-//         }
-//       }
-//     });
-//   });
-// }
-
-// arithmetic();
-
 function getNumber(num) {
     inputfield.value += num;
 }
 
 function getOperator(operator) {
     if (operator === ".") {
-        // Check if decimal point already exists
         if (!inputfield.value.includes(".")) {
             inputfield.value += operator;
         }
     } else {
+        console.log(inputfield.value);
         inputfield.value += " " + operator + " ";
     }
 }
@@ -81,11 +30,16 @@ function clearitem() {
 
 function Calculate() {
     const expression = inputfield.value;
+    console.log(expression);
     const elements = expression.split(" ");
     console.log(elements);
 
     let result = parseFloat(elements[0]);
     for (let i = 1; i < elements.length; i += 2) {
+        if (elements[i + 1] === "" || elements[i].includes("Error")) {
+            console.log(elements[i + 1] === "", elements[i + 1]);
+            return inputfield.value = "Error"
+        }
         const operator = elements[i];
         const operand = parseFloat(elements[i + 1]);
 
